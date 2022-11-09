ddced41803cf40fbd2c0caebfecff1c271e8ca84
Joe Fogiato <joe.fogiato@gmail.com>
2022-11-09 16:55:50 -0500

    Add average friend

--- friends.js
+++ friends.js
@@ -1,7 +1,12 @@
 var bestFriend = "Stan"
+var avgFriend = "Joe"
 
 if (bestFriend === "Jay") {
     console.log("You mean a lot to me!")
-} else { 
-    console.log("You are still okay.")
+} else if (avgFriend === "Brandon") {
+    console.log("You're cool too Brandon")
+} else {     console.log("You are still okay.")
 }
+
+
+
